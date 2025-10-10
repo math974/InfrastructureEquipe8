c6b0b6138984d1d49dc2aa0e329af36537de483a
math974 <mathias.ballot@epitech.eu>
2025-10-10 11:22:14 +0200

    feat(CI/CD): workload identity test pipeline CI/CD

---
+++ bootstrap-wif/variables.tf
@@ -0,0 +1,56 @@
+variable "project_id" {
+  description = "ID du projet GCP"
+  type        = string
+}
+
+variable "pool_id" {
+  description = "ID du Workload Identity Pool"
+  type        = string
+  default     = "github-pool"
+}
+
+variable "provider_id" {
+  description = "ID du Workload Identity Provider"
+  type        = string
+  default     = "github"
+}
+
+variable "service_account_id" {
+  description = "ID (name) du service account Terraform"
+  type        = string
+  default     = "github-terraform"
+}
+
+variable "service_account_display_name" {
+  description = "Nom d'affichage du service account"
+  type        = string
+  default     = "GitHub Terraform"
+}
+
+variable "github_owner" {
+  description = "Organisation ou utilisateur GitHub"
+  type        = string
+}
+
+variable "github_repo" {
+  description = "Nom du dépôt GitHub"
+  type        = string
+}
+
+variable "branch_excluded" {
+  description = "Nom de la branche à exclure (ex: master)"
+  type        = string
+  default     = "master"
+}
+
+variable "roles" {
+  description = "Liste des rôles à attribuer au service account"
+  type        = list(string)
+  default = [
+    "roles/storage.admin",
+    "roles/compute.admin",
+    "roles/iam.serviceAccountUser",
+    "roles/viewer",
+  ]
+}
+
